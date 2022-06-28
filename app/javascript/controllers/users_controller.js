d1803e1eea14d84c18122a13b2e8fef9c9642ccc
Mael Harnois <maelh@MBP-MAEL.local>
2022-06-28 12:40:29 +0200

    implemented users_controller.js

--- app/javascript/controllers/users_controller.js
+++ app/javascript/controllers/users_controller.js
@@ -1,50 +1,46 @@
-// Visit The Stimulus Handbook for more details 
-// https://stimulusjs.org/handbook/introduction
-// 
-// This example controller works with specially annotated HTML like:
-//
-// <div data-controller="hello">
-//   <h1 data-target="hello.output"></h1>
-// </div>
-
 import { Controller } from "@hotwired/stimulus"
 
 export default class extends Controller {
   static targets = [ "buttonEthConnect", "inputEthAddress", "formNewUser" ]
 
   connect() {
-    this.outputTarget.textContent = 'Hello, Stimulus!'
+    console.log("in users_controller.js")
   }
 
-  ethConnect() {
-        // the button to connect to an ethereum wallet
-    const buttonEthConnect = document.querySelector('button.eth_connect');
+  tryEthConnect() {
+    console.log("in tryEthConnect")
+    // the button to connect to an ethereum wallet
+    const buttonEthConnect = this.buttonEthConnectTarget
 
     // the read-only eth address field, we process that automatically
-    const formInputEthAddress = document.querySelector('input.eth_address');
-    formInputEthAddress.hidden = true;
+    const inputEthAddress = this.inputEthAddressTarget
+    inputEthAddress.hidden = true;
 
     // get the user form for submission later
-    const formNewUser = document.querySelector('form.new_user');
+    const formNewUser = this.formNewUserTarget
 
     // only proceed with ethereum context available
     if (typeof window.ethereum !== 'undefined') {
-      buttonEthConnect.addEventListener('click', async () => {
-        buttonEthConnect.disabled = true;
-
-        // request accounts from ethereum provider
-        const accounts = await this.requestAccounts();
-        const etherbase = accounts[0];
-
-        // populate and submit form
-        formInputEthAddress.value = etherbase;
-        formNewUser.submit();
-    });
+      console.log("window.ethereum OK")
+      this.ethConnect()
     } else {
+      console.log("window.ethereum NOT OK")
       // disable form submission in case there is no ethereum wallet available
       buttonEthConnect.innerHTML = "No Ethereum Context Available";
       buttonEthConnect.disabled = true;
     }
+  }
+
+  async ethConnect() {
+    // get the user form for submission later
+    const formNewUser = this.formNewUserTarget
+    this.buttonEthConnectTarget.disabled = true;
+    // request accounts from ethereum provider
+    const accounts = await this.requestAccounts();
+    const etherbase = accounts[0];
+    // populate and submit form
+    this.inputEthAddressTarget.value = etherbase;
+    formNewUser.submit();
   }
 
   // request ethereum wallet access and approved accounts[]
